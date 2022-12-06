571e41eba0c35097f02877d9f7cc4c28eb8cb9bf
anaijg <ana.jimenezgomez@educa.madrid.org>
2022-12-06 08:15:36 +0100

    Problema 532 - ana - aceptado

--- src/P532.java
+++ src/P532.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 /**
  * @see <a href="https://www.aceptaelreto.com/problem/statement.php?id=532&cat=125">Reduciendo envases</a>
  */
 public class P532 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int casosDePrueba = sc.nextInt(); sc.nextLine();
+        for (int i = 0; i < casosDePrueba; i++) {
+            String linea = sc.nextLine();
+            String[] array = linea.split(" ");
+            int N = Integer.parseInt(array[0]);
+            int T = Integer.parseInt(array[1]);
+            int envase = T - N;
+            System.out.println(envase);
+        }
     }
 }
