e4e9abdc0e5d9f092b8a5ab3390ad049817bd1c6
anaijg <ana.jimenezgomez@educa.madrid.org>
2022-12-06 19:11:23 +0100

    Problema 535 - ana - aceptado

--- src/P535.java
+++ src/P535.java
@@ -1,8 +1,40 @@
+import java.util.Scanner;
+
 /**
  * @see <a href="https://www.aceptaelreto.com/problem/statement.php?id=535&cat=125">Desembalse</a>
  */
+
 public class P535 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int D = 0;
+        String linea = "";
+        do {
+            D = sc.nextInt(); sc.nextLine();
+            if (D != 0) {
+                linea = sc.nextLine();
+                String[] array = linea.split(" ");
+                int[] alturas = new int[D];
+                for (int i = 0; i < array.length; i++) {
+                    alturas[i] = Integer.parseInt(array[i]);
+                }
+                // este problema se entiende mejor haciendo un dibujo
+                // encontrar la altura mayor
+                int mayor = 0;
+                for (int i = 0; i < alturas.length; i++) {
+                    if (alturas[i] > mayor) {
+                        mayor = alturas[i];
+                    }
+                }
+                // restamos la altura menor a todas las demás
+                // y vamos sumando las alturas
+                int suma = 0;
+                for (int i = 0; i < alturas.length; i++) {
+                    suma = suma + (mayor - alturas[i]);
+                }
+                // finalmente, mostramos la suma, que es lo que nos piden
+                System.out.println(suma);
+            }
+        } while (D != 0);
     }
 }
