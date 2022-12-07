8cf1fff016d0de77bcea09e142b34e3a903bcbdd
anaijg <ana.jimenezgomez@educa.madrid.org>
2022-12-07 10:03:57 +0100

    Cucharadas de sopa

--- src/P577.java
+++ src/P577.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 /**
  * @see <a href="https://www.aceptaelreto.com/problem/statement.php?id=577&cat=143">Cucharadas de sopa</a>
  */
 public class P577 {
     public static void main(String[] args) {
+        // Crea un objeto Scanner para leer la entrada
+        Scanner scanner = new Scanner(System.in);
 
+        // Lee el número de casos de prueba
+        int n = scanner.nextInt();
+
+        // Procesa cada caso de prueba
+        for (int i = 0; i < n; i++) {
+            // Lee los tres números que indican cuántas cucharadas dice Mafalda que se ha tomado como mínimo,
+            // cuántas cucharadas dice el padre que se ha tomado como máximo, y cuántas cucharadas había puesto
+            // realmente en el plato la madre.
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            System.out.println( Math.min(c - a + 1, b + 1) );
+        }
     }
 }
